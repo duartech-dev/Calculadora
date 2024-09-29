28bd6a77bd4c4608f94d548aa981b487013ccf1b
duartech-dev <https://github.com/duartech-dev/node.git>
2024-09-29 12:19:16 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const display = document.querySelector("#display");
+const buttons = document.querySelectorAll("button")
+buttons.forEach(btn => {
+    btn.addEventListener("click", () => {
+        if (btn.id === "igual") {
+            display.value = eval(display.value);
+        } else if (btn.id === "AC") {
+            display.value = "";
+        } else if (btn.id === "C") {
+            display.value = display.value.slice(0, -1);
+        } else {
+            display.value += btn.id;
+        }
+    });
+});
